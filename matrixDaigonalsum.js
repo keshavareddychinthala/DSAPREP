593ca6d6b689b60fc60734d01e8d344792304335
keshavareddy <48752396+keshavareddychinthala@users.noreply.github.com>
2022-11-10 17:00:34 +0000

    updated Matrix daigonal sum

---
+++ matrixDaigonalsum.js
@@ -0,0 +1,15 @@
+var diagonalSum = function(mat) {
+    let sum = 0;
+       for(let i=0;i<mat.length;i++){
+           if(i==mat.length-i-1){
+               sum+=mat[i][i];
+           }
+           else{
+               sum+=mat[i][i]+mat[i][mat.length-i-1];
+           }
+       }
+      
+      return sum;  
+  };
+  
+
